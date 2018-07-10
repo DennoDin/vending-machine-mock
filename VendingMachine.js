091c12bc3215d894f1567a705740600cda92bea3
Code Chrysalis <codechrysalis@Mac-mini-2.local>
2018-07-10 17:16:28 +0900

    add coin return function

--- VendingMachine.js
+++ VendingMachine.js
@@ -9,7 +9,7 @@ const inventory = require("./inventory");
 
 class VendingMachine {
   constructor() {
-    this.till = { "10": 0, "50": 0, "100": 0, "500": 0 };
+    this.till = { "10": 10, "50": 10, "100": 10, "500": 10 };
     this.balance = 0;
     this.row;
     this.column;
@@ -45,8 +45,45 @@ class VendingMachine {
         btnNum = 3;
       }
       console.log(this.row + this.column);
-      if (inventory[btnNum][this.column - 1].count > 0) {
+      let cost = inventory[btnNum][this.column - 1].price;
+      if (
+        inventory[btnNum][this.column - 1].count > 0 &&
+        this.balance >= cost
+      ) {
         --inventory[btnNum][this.column - 1].count;
+        console.log("Here is your " + inventory[btnNum][this.column - 1].name);
+        let change = this.balance - cost;
+        let coins = [0, 0, 0, 0];
+        while (change > 0) {
+          if (change >= 500) {
+            this.till["500"]--;
+            coins[0]++;
+            change = change - 500;
+          } else if (change >= 100) {
+            this.till["100"]--;
+            coins[1]++;
+            change = change - 100;
+          } else if (change >= 50) {
+            this.till["50"]--;
+            coins[2]++;
+            change = change - 50;
+          } else if (change >= 10) {
+            this.till["10"]--;
+            coins[3]++;
+            change = change - 10;
+          }
+        }
+        this.balance = 0;
+        console.log(
+          "500: " +
+            coins[0] +
+            ", 100: " +
+            coins[1] +
+            ", 50: " +
+            coins[2] +
+            ", 10: " +
+            coins[3]
+        );
       }
     }
   }
